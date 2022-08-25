b7e46a3cdc078469a52687c37a2d18eef7d70eba
Pitson3 <josiahprojects1@gmail.com>
2022-08-25 21:03:08 +0000

    Filled the run_kuberntes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=pitson3/flask-ml
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run flask-ml --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward flask-ml 8000:80 
 
